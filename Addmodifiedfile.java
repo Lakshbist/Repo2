cf3b1976ea221007d5b694ffa40159aa21f52ff4
lakshbist <lakshbist2000@gmail.com>
2023-03-26 18:07:52 +0530

    This is the program to add two numbers

---
+++ Addmodifiedfile.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Addmodifiedfile
+
+{
+    public static void main(String args[])  {
+       System.out.println("Enter the two numbers to be added");
+         Scanner sc = new Scanner(System.in);
+           int firstnum = sc.nextInt();
+           int secondnum = sc.nextInt();
+           int result = firstnum + secondnum;
+          System.out.println("The sum of two numbers is" + " " result);
+}
+}
+
+         
+               
