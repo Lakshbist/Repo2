45302a40620ff5ed82d95926785984b54fc572e6
Lakshmi Bist <127615998+Lakshbist@users.noreply.github.com>
2023-03-26 20:32:47 +0530

    Add files via upload

---
+++ Evenandodd.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Evenandodd {
+                        
+           public static void main(String args[]) {
+               System.out.println("Enter the number");
+               Scanner sc = new Scanner(System.in);
+               int num = sc.nextInt();
+               
+                if (num / 2 == 0) {
+                       System.out.println("Number is even" + " " + num );
+                  else {
+                          System.out.println("Number is odd" + " " + num);
+                        }
+                 }
+ }
+}
+
+                                
